70eb5657fd25f46858868fe1690acb13d50b5b1c
Ngo Hai Minh <ngominh9499@gmail.com>
2024-11-13 15:15:03 +0700

    them phan hien ten nguoi dung trong phan quyen admin

--- src/main/java/vn/nmcnpm/quanlysinhvien/repository/StudentRepository.java
+++ src/main/java/vn/nmcnpm/quanlysinhvien/repository/StudentRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.nmcnpm.quanlysinhvien.domain.Student;
+import vn.nmcnpm.quanlysinhvien.domain.User;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    public Student findByUser(User user);
 }
